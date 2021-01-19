804426a909c865796d1d9a1ba4aea10534c11df5
Franck <franck@mail.com>
2021-01-19 16:26:12 +0100

    utilisation des icon de openweathermap

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var express = require('express');
 var router = express.Router();
 
 let cityList = [
-  { nom: 'Paris', image: 'fa-cloud-sun', descriptif: 'nuageux', tmin: 5, tmax: 12},
-  { nom: 'Lyon', image: 'fa-cloud', descriptif: 'ciel dégagé', tmin: 7, tmax: 14},
-  { nom: 'Toulouse', image: 'fa-sun', descriptif: 'tempête', tmin: 5, tmax: 12},
-  { nom: 'Bordeaux', image: 'fa-snowflake', descriptif: 'ouragan', tmin: 7.5, tmax: 14.4},
+  { nom: 'Paris', icon: '04d', descriptif: 'nuageux', tmin: 5, tmax: 12},
+  { nom: 'Lyon', icon: '01d', descriptif: 'ciel dégagé', tmin: 7, tmax: 14},
+  { nom: 'Toulouse', icon: '01d', descriptif: 'tempête', tmin: 5, tmax: 12},
+  { nom: 'Bordeaux', icon: '03d', descriptif: 'ouragan', tmin: 7.5, tmax: 14.4},
 ]
 
 const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?appid=b49185709cdd24c630699b40eaa3d7ed&units=metric&lang=fr'
@@ -26,7 +26,7 @@ const modifyList = (citiesList, todo, el) => {
   if (!find && todo === 'add' && el.nom !=='') {
     citiesList.push({
       nom: el.nom,
-      image: el.image,
+      icon: el.icon,
       descriptif: el.descriptif,
       tmin: el.tmin,
       tmax: el.tmax,
@@ -42,7 +42,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/weather', function(req, res, next) {
-  res.render('weather', { title: 'WeatherApp', cityList });
+  res.render('weather', { title: 'WeatherApp', cityList, cityNotFound: false });
 });
 
 router.get('/add-city', function(req, res, next) {
@@ -51,14 +51,15 @@ router.get('/add-city', function(req, res, next) {
   var result = request("GET", weatherURLCity)
 
   var resultJSON = JSON.parse(result.body)
+  // console.log(resultJSON)
 
   if(resultJSON.cod == '404') {
     cityNotFound = true
   } else {
     cityList = modifyList(cityList, 'add', {
       nom: req.query.city,
-      image: 'fa-cloud',
-      descriptif: resultJSON.weather.description,
+      icon: resultJSON.weather[0].icon,
+      descriptif: resultJSON.weather[0].description,
       tmin: resultJSON.main.temp_min,
       tmax: resultJSON.main.temp_max,
     })
@@ -68,7 +69,7 @@ router.get('/add-city', function(req, res, next) {
 
 router.get('/delete-city', function(req, res, next) {
   cityList = modifyList(cityList, 'delete', req.query)
-  res.render('weather', { title: 'WeatherApp', cityList });
+  res.render('weather', { title: 'WeatherApp', cityList, cityNotFound: false });
 });
 
 module.exports = router;
